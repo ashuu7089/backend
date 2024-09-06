b698caa521b30bac3d5fe102b0f89feac1bd97cb
Developermactosys <developermactosys@gmail.com>
2024-09-06 14:21:11 +0530

    add utils file update dbConfig.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // require('dotenv').config({path : './env'})
 
 import dotenv from 'dotenv';
-import connectDB from './db/dbConfig';
+import connectDB from './db/dbConfig.js';
+import { app } from './app.js';
 
 dotenv.config({
     path : './env'
 })
 
 connectDB()
+.then((response)=>{
+    app.listen(process.env.PORT || 8500,  ()=> {
+        console.log(`Server running on port : ${process.env.PORT }`);
+    })
+})
+.catch((error)=> {
+    console.log("MongoDB connection Error : ", error);
+    throw error;
+})
---
+++ src/utils/apiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went to wrong",
+        errors = [],
+        stack = ''
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.status = false,
+        this.errors = errors
+        
+        if(stack) {
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiError }
